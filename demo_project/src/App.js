b358b644d862e997837e1ed6f01b41bc9ba6b46a
DrakshayaniB <drakshayani.b@innomick.com>
2020-09-28 20:06:14 +0530

    added firebase setup

---
+++ demo_project/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import  firebase from 'firebase';
+import "firebase/database";
+
+function App() {
+  console.log(firebase);
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
